fab91f7a734f1d7dff107a2511ece1acbdeaf145
Almir M <almir.mutapcic@ssst.edu.ba>
2012-05-16 01:33:02 +0200

    Initial import of the test class for github.

---
+++ src/TestClass.java
@@ -0,0 +1,12 @@
+
+public class TestClass {
+
+	/**
+	 * This is a simple test class for github integration.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+
+}
