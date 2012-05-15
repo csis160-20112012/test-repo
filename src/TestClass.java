7b200cab79dc93b371cfc5afe34bd6180378e39e
Almir M <almir.mutapcic@ssst.edu.ba>
2012-05-16 01:40:45 +0200

    Added Github to hello world message.

--- src/TestClass.java
+++ src/TestClass.java
@@ -6,7 +6,7 @@ public class TestClass {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("Hello world is on Github");
 	}
 
 }
